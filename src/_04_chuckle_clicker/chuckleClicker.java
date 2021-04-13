881d71fd6419cfa25ba3fedc9ed43c8d2a540a7a
zoesalinas <zoesalinas@najdorf.com>
2021-04-12 18:29:25 -0700

    Chuckle Clicker

--- src/_04_chuckle_clicker/chuckleClicker.java
+++ src/_04_chuckle_clicker/chuckleClicker.java
@@ -1,9 +1,30 @@
 package _04_chuckle_clicker;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class chuckleClicker {
-public static void main(String[] args) {
-	
-	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JButton button2 = new JButton();
+
+chuckleClicker(){
+	frame.setVisible(true);
+	frame.add(panel);
+	makeButtons();
+	frame.pack();
+}
+
+private void makeButtons() {
+	// TODO Auto-generated method stub
+//JOptionPane.showMessageDialog(null, "Make Buttons");
+	panel.add(button);
+	panel.add(button2);
+	button.setText("joke");
+	button.setText("punchline");
 	
 }
 }
---
+++ src/_04_chuckle_clicker/chuckleClickerRunner.java
@@ -0,0 +1,7 @@
+package _04_chuckle_clicker;
+
+public class chuckleClickerRunner {
+	public static void main(String[] args) {
+		new chuckleClicker();
+	}
+}
