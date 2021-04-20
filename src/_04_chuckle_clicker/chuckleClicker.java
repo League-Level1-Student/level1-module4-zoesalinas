6ccb9c153aa7f2313ba5e9249b2244f1eb79d39c
zoesalinas <zoesalinas@najdorf.com>
2021-04-19 18:29:27 -0700

    Typing Tutor

--- src/_04_chuckle_clicker/chuckleClicker.java
+++ src/_04_chuckle_clicker/chuckleClicker.java
@@ -1,11 +1,14 @@
 package _04_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class chuckleClicker {
+public class chuckleClicker implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button = new JButton();
@@ -24,7 +27,22 @@ private void makeButtons() {
 	panel.add(button);
 	panel.add(button2);
 	button.setText("joke");
-	button.setText("punchline");
+	button2.setText("punchline");
+	button.addActionListener(this);
+	button2.addActionListener(this);
 	
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	JButton buttonPressed = (JButton) e.getSource();
+	if(buttonPressed == button) {
+		JOptionPane.showMessageDialog(null, "Yesterday I bought some powdered water...");
+		
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "I spent the whole day trying to figure out what to add to it.");
+	}
 }
 }
